60ac1af885d0c7bc23c2cbfa5ac0c228ed8dafb5
xacrucesalus <zackjcooper@gmail.com>
2016-08-16 14:04:43 -0400

    init commit

---
+++ stockapp/views.py
@@ -0,0 +1,57 @@
+from flask import Blueprint, flash, render_template
+import json, os, requests
+from multiprocessing import Pool              # For speeding up WS to S3 process
+from multiprocessing.dummy import Pool as ThreadPool
+
+
+stockapp = Blueprint("stockapp", __name__)
+API_KEY = os.environ['API_KEY']
+
+def flash_errors(form, category="warning"):
+    '''Flash all errors for a form.'''
+    for field, errors in form.errors.items():
+        for error in errors:
+            flash("{0} - {1}"
+                    .format(getattr(form, field).label.text, error), category)
+
+
+@stockapp.route("/<ticker>")
+def index(ticker):
+    ticker = ticker.upper()
+    ticker_dict = get_ticker_info(ticker)
+    return render_template('index.html', ticker_dict=ticker_dict)
+
+
+def get_ticker_info(ticker):
+    """Takes in a stock ticker string and returns a dict of urls and text
+    describing those urls."""
+    with open(os.path.dirname(os.path.abspath(__file__)) + "/raw_data.json") as f:
+        lines = json.load(f)
+
+    for match, values in lines.items():
+        if match == ticker:
+            ticker_info = {'ticker': ticker, 'company_name': values['company_name']}
+            ticker_info['chart_one_narrative'] = wordsmith(values['chart_one_data'],
+                                                           "simplr-stock-price")
+            ticker_info['chart_two_narrative'] = wordsmith(values['chart_two_data'],
+                                                           "simplr-eps")
+            ticker_info['chart_three_narrative'] = wordsmith(values['chart_three_data'],
+                                                             "simplr-net-income")
+            ticker_info['chart_four_narrative'] = wordsmith(values['chart_four_data'],
+                                                            "simplr-sector-comparison")
+            return ticker_info
+
+
+def wordsmith(data_dict, project):
+    """Takes in data set to call out to Wordsmith API for a narrative"""
+
+    url = "https://api.automatedinsights.com/v1/projects/" + project + \
+          "/templates/new-template/outputs"
+    headers = {'Authorization': 'Bearer ' + API_KEY,
+               'Content-Type': 'application/json',
+               'User-Agent': 'Hackathon Application'}
+    data = json.dumps({"data": data_dict})
+
+    response = requests.post(url, headers=headers, data=data)
+    print(response.json())
+    return response.json()['data']['content']
